6b31bc8ebd96e69d7e65278349a7935260304572
taivnaaiwnl <taivnaaiwnl@gmail.com>
2024-03-16 21:49:04 +0800

    last commit

--- main.py
+++ main.py
@@ -2,26 +2,29 @@ from ultralytics import YOLO
 from ultralytics.solutions import object_counter
 import cv2
 
+# model = YOLO("yolov8n.pt")
+# cap = cv2.VideoCapture(0)
+# assert cap.isOpened()
+# w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+
+
+#Хэрэглэгч өөрийн дата дээр туршихыг хүсвэл
 model = YOLO("yolov8n.pt")
-cap = cv2.VideoCapture(0)
-assert cap.isOpened(), "Error opening video stream or file"
+cap = cv2.VideoCapture("test.mp4")
+assert cap.isOpened(), "Error reading video file"
 w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
-# Define line points
 line_points = [(20, 400), (1080, 400)]
 
-# Video writer
-video_writer = cv2.VideoWriter("object_counting_output.mp4",
-                       cv2.VideoWriter_fourcc(*'mp4v'),
-                       fps,
-                       (w, h))
+video_writer = cv2.VideoWriter("output.mp4",
+                               cv2.VideoWriter_fourcc(*'mp4v'),
+                               fps,
+                               (w, h))
 customname = {0: 'hvn'}
-print(model.names)
-# Init Object Counter
 counter = object_counter.ObjectCounter()
 counter.set_args(view_img=True,
                  reg_pts=line_points,
-                 classes_names=customname,
+                 classes_names=model.names,
                  draw_tracks=True)
 
 while cap.isOpened():
@@ -35,7 +38,6 @@ while cap.isOpened():
 
     video_writer.write(im0)
 
-    # q үсэг дээр дарж камерыг хаана
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
