9217ec2be1ca89a9f43f76a120fc1646e6892153
taivnaaiwnl <taivnaaiwnl@gmail.com>
2024-03-16 21:49:39 +0800

    last1 commit

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from ultralytics import YOLO
 from ultralytics.solutions import object_counter
 import cv2
 
-# model = YOLO("yolov8n.pt")
-# cap = cv2.VideoCapture(0)
-# assert cap.isOpened()
-# w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+model = YOLO("yolov8n.pt")
+cap = cv2.VideoCapture(0)
+assert cap.isOpened()
+w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
 
 #Хэрэглэгч өөрийн дата дээр туршихыг хүсвэл
-model = YOLO("yolov8n.pt")
-cap = cv2.VideoCapture("test.mp4")
-assert cap.isOpened(), "Error reading video file"
-w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+# model = YOLO("yolov8n.pt")
+# cap = cv2.VideoCapture("test.mp4")
+# assert cap.isOpened(), "Error reading video file"
+# w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
 line_points = [(20, 400), (1080, 400)]
 
